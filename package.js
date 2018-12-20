6c20bc33365a25e19491a8ac0e81b026601a0ffe
Dane Truelson <me@Danes-MacBook-Pro.local>
2018-12-19 20:56:24 -0600

    work

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'barbatus:typescript',
+  name: 'turbobuilt:typescript',
   version: '0.7.0',
   summary: 'TypeScript for Meteor',
   git: 'https://github.com/barbatus/typescript',
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: 'typescript',
-  use: ['barbatus:typescript-compiler'],
+  use: ['turbobuilt:typescript-compiler'],
   sources: ['plugin.js']
 });
 
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('barbatus:typescript-compiler@0.10.0');
+  api.use('turbobuilt:typescript-compiler@0.10.0');
 
   api.imply('modules@0.11.6');
 
@@ -25,7 +25,7 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest@1.0.12');
-  api.use('barbatus:typescript');
+  api.use('turbobuilt:typescript');
 
   api.addFiles('tests/runtime-tests.ts', 'client');
   api.addFiles('tests/runtime-react-tests.tsx', 'client');
